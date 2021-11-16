4c8482e1482b0937746716bebeb1c4c2a62059bd
Ari Chadda <ari.chadda@gmail.com>
2021-11-16 11:55:31 -0500

    safari updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import vid from './vid.mp4'
 function App() {
   return (
     <div className="App">
-      <video autoPlay muted height='85%' width='85%'>
+      <video autoPlay height='85%' width='85%'>
         <source src= { vid } type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
       </video>
     </div>
