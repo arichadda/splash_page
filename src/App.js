0c2b36109c5f9ec4c0d901f559cc250c7b74c766
Ari Chadda <ari.chadda@gmail.com>
2021-11-16 12:01:53 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -13,9 +13,6 @@ function App() {
           </video>`,
           }}
         />
-      {/* <video autoPlay muted height='85%' width='85%'>
-        <source src= { vid } type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
-      </video> */}
     </div>
   );
 }
